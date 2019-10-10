9120b5e40c36f6e7159d76a93f6d7c496c15bf90
yOpenSource <yogeshhirachandmali@gmail.com>
2019-10-10 23:46:59 +0530

    async function added in " fetch async await/index.js " returns a promise - having fun with it

--- 0002fetch async await/index.js
+++ 0002fetch async await/index.js
@@ -3,10 +3,19 @@ console.log(" going to fetch a image from server");
  async function get_wooden_table(){
    let res =  await fetch('wooden_table.jpg');
    let blob = await res.blob();
-   document.getElementById('wooden_tble').src=URL.createObjectURL(blob);
+   document.getElementById('wooden_table').src=URL.createObjectURL(blob);
 }
 
-get_wooden_table().catch(
+//async function also returns a promise
+//so we can use .then after the async function call
+
+get_wooden_table()
+.then(
+  response => {
+    console.log("just having fun with it");
+  }
+)
+.catch(
   error => {
     console.log('Caught Error :');
     console.error(error);
