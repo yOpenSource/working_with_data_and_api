e406e76fdc519ba37a147d0f022d01a1276a65f1
yOpenSource <yogeshhirachandmali@gmail.com>
2019-10-10 23:42:33 +0530

    async function added in " fetch async await/index.js "

--- 0002fetch async await/index.js
+++ 0002fetch async await/index.js
@@ -1,5 +1,14 @@
-    console.log(" going to fetch a image from server");
+console.log(" going to fetch a image from server");
 
+ async function get_wooden_table(){
    let res =  await fetch('wooden_table.jpg');
    let blob = await res.blob();
-   document.getElementById('wooden_table').src=URL.createObjectURL(blob);
+   document.getElementById('wooden_tble').src=URL.createObjectURL(blob);
+}
+
+get_wooden_table().catch(
+  error => {
+    console.log('Caught Error :');
+    console.error(error);
+  }
+);
